c4cc0c4a2f2ff79bce2ed136e15721b32f9adef6
JO HUI JI <whgmlwl222@naver.com>
2021-02-22 17:05:26 +0900

    3장 완료

---
+++ chap03/Chap03App/ConstTestApp/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ConstTestApp
+{
+    class Program
+    {
+        enum Season // 변수명(시즌타입)
+        {
+            봄,
+            여름,
+            가을,
+            겨울
+
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("상수테스트");
+            const double PI = 3.141592;
+            Console.WriteLine($"원주율의 값은 {PI}");
+            // PI = 6.345; 실행부터 오류남
+
+            Season mySeason = Season.겨울; //(Season) 3; 할 수도 있지만 위에 열거형 값이 변경된다면(겨울 = 200) 3을 쓸 수 없다
+            Console.WriteLine($"지금 계절은 {mySeason}입니다");
+
+            int a = 0; // null;
+            Console.WriteLine($"a는 {a}");
+            int? b = null;
+            Console.WriteLine($"b는 {b}");
+        }
+    }
+}
---
+++ chap03/Chap03App/VarTestApp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VarTestApp
+{
+    class Program
+    {
+        // var i = 100; 지역변수이므로 메인메서드 내부에서만 쓰임 형변환 필요없음
+        static void Main(string[] args)
+        {
+            var i = 100;
+            Console.WriteLine($"정수값은 {i}입니다.");
+        }
+    }
+}
