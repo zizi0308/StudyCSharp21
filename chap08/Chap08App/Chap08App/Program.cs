71ecfa6f08c1bb05036dd2ac8559fad9a13c8672
JO HUI JI <whgmlwl222@naver.com>
2021-02-25 15:22:31 +0900

    8장 완료

---
+++ chap08/Chap08App/Chap08App/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chap08App
+{
+    interface ILogger
+    {
+        void WriteLog(string msg);
+    }
+
+    class ConsoleLogger : ILogger
+    {
+        public void WriteLog(string msg)
+        {
+            Console.WriteLine($"{DateTime.Now} log : {msg}");
+        }
+    }
+
+    class CustomLogger : ILogger
+    {
+        public void WriteLog(string msg)
+        {
+            Console.WriteLine($"Log : {msg}\a\a");
+        }
+    }
+    class ClimateLogger
+    {
+        private ILogger logger;
+        public ClimateLogger(ILogger logger)
+        {
+            this.logger = logger;
+        }
+
+        public void Start()
+        {
+            while (true)
+            {
+                Console.Write("온도를 입력 : ");
+                string temp = Console.ReadLine();
+                if (string.IsNullOrEmpty(temp)) break;
+
+                logger.WriteLog($"현재온도 : {temp}");
+            }
+        }
+
+    }
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            // ILogger logger = new ILogger(); >> 인터페이스 자체로는 형변환 못함
+            // ILogger를 구현해서 가져와 쓰는것은 가능함 아까 강아지 부모클래스에 자식인스턴스 만들어쓴 것처럼
+            //ILogger logger = new ConsoleLogger(); << 아래와 동일하게 쓰임
+            ConsoleLogger logger = new ConsoleLogger();
+            logger.WriteLog("로그출력"); // 내가 직접 로그를 찍음
+
+            ClimateLogger clogger = new ClimateLogger(new CustomLogger()); //ClimateLogger가 미리 만들어 졌다고 생각해야 함
+            clogger.Start();// 우리가 안하고 프레임워크가 실행시켜주는것 IoC(제어의 역흐름)  우리는 Start만 실행시켜주면 됨
+        }
+    }
+}
---
+++ chap08/Chap08App/MultiInterfaceApp/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace MultiInterfaceApp
+{
+
+    interface IRunnable // 자동차 종류
+    {
+        void Run();
+    }
+
+    interface IFlyable // 비행기 종류
+    {
+        void Fly();
+    }
+
+    class DroneCar : IRunnable, IFlyable
+    {
+        public void Fly()
+        {
+            Console.WriteLine("날아!");
+        }
+
+        public void Run()
+        {
+            Console.WriteLine("달려!");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("드론카!");
+
+            DroneCar dreamCar = new DroneCar();
+            dreamCar.Run();
+            dreamCar.Fly();
+
+
+            Console.WriteLine("자동차로 변경(Interface)");
+            IRunnable car = dreamCar;
+            car.Run();
+            // car.Fly(); << err 부모가 자식으로 바뀔 수없다
+
+
+            Console.WriteLine("비행기로 변경(Interface)");
+            IFlyable plain = dreamCar;
+            plain.Fly();
+            // plain.Run(); << err 부모가 자식으로 바뀔 수없다
+
+        }
+    }
+}
