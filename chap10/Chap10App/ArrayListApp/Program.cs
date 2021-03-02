ea00863da5910dc801663de60abb90a9f08738d1
JO HUI JI <whgmlwl222@naver.com>
2021-03-02 10:21:51 +0900

    10장 완료

---
+++ chap10/Chap10App/ArrayListApp/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayListApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList array = new ArrayList(new[] { 80, 74, 81, 90, 34 });
+            /*array.Add(80);
+            array.Add(74);
+            array.Add(81);
+            array.Add(90);
+            array.Add(34);*/
+
+            var loc = array.IndexOf(81);
+            array.Insert(loc, 50); // 2번째 인덱스에 50을 넣어라(인덱스 0부터 시작 3번째에 50들어감)
+
+            Console.WriteLine("81 위치에 50추가");
+            foreach (var item in array)
+            {
+                Console.WriteLine($" {item}");
+            }
+
+            Console.WriteLine("90 삭제");
+            array.RemoveAt(4); // 90 삭제
+            foreach (var item in array)
+            {
+                Console.WriteLine($" {item}");
+            }
+
+            Console.WriteLine("정렬");
+            array.Sort();
+            foreach (var item in array)
+            {
+                Console.WriteLine($" {item}");
+            }
+        }
+    }
+}
---
+++ chap10/Chap10App/HashTableApp/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+
+namespace HashTableApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("HashTable 예제");
+
+            Hashtable ht = new Hashtable();
+            ht["일"]/*Table컬럼명*/ = "One";
+            ht["이"] = "Two";
+            ht["삼"] = "Three";
+            ht["사"] = "Four";
+
+            Console.WriteLine(ht["일"]); // One
+            Console.WriteLine(ht["이"]); // Two
+            Console.WriteLine(ht["사"]); // Four
+
+            foreach (DictionaryEntry item in ht)
+            {
+                Console.WriteLine($"{item.Key} : {item.Value}"); // 정렬하는 데이터가 아님(콘솔창에 순서 계속 바뀜)
+            }
+
+            int[] arr = { 123, 456, 789 };
+            ArrayList list = new ArrayList(arr);
+            ArrayList list2 = new ArrayList(new int[] { 123, 456, 789 });
+            ArrayList list3 = new ArrayList() { 123, 456, 789 }; // 셋다 같은 결과 나옴
+        }
+    }
+}
