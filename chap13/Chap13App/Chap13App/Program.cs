e58c0fd2318c5daeb259bda4291fd4acca0a6a21
JO HUI JI <whgmlwl222@naver.com>
2021-03-02 17:25:41 +0900

    13장 대리자 커밋

---
+++ chap13/Chap13App/Chap13App/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chap13App  // WinForm 작업시 이벤트를 처리하기 위해 Delegate를 사용한다.
+{
+    delegate int MyDelegate(int a, int b); // 대리자 >> 대신호출 // 호출할 메서드와 파라미터 타입 개수가 일치해야한다.
+    class Calculator
+    {
+        public int Plus(int a, int b)
+        {
+            return a + b;
+        }
+
+        public int Minus(int a, int b)
+        {
+            return a - b;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator calc = new Calculator();
+            MyDelegate Callback;  // 호출자가 콜백을 호출
+
+            Callback = new MyDelegate(calc.Plus); // 대리자가 calc.Plus라는 메서드를 호출함
+            Console.WriteLine($"result = {Callback(3, 4)}");
+
+            Callback = new MyDelegate(calc.Minus);
+            Console.WriteLine($"result = {Callback(5, 2)}");
+        }
+    }
+}
---
+++ chap13/Chap13App/DelegateChainApp/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace DelegateChainApp // 대리자 하나만 호출하면 다 호출가능
+{
+    delegate void AllCalc(int x, int y); // 대리자 선언
+    class Program
+    {
+        static void Plus(int a, int b) { Console.WriteLine($"a + b = {a + b}"); }
+        static void Minus(int a, int b) { Console.WriteLine($"a - b = {a - b}"); }
+        static void Multiple(int a, int b) { Console.WriteLine($"a * b = {a * b}"); }
+        static void Divide(int a, int b) { Console.WriteLine($"a / b = {a / b}"); }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Calculating!");
+            AllCalc allCalc = Plus;
+            allCalc += Minus; // += << delegate chain
+            allCalc += Multiple;
+            allCalc += Divide;
+
+            allCalc(10, 5);
+            /*Plus(10, 5);
+            Minus(10, 5);
+            Multiple(10, 5);
+            Divide(10, 5);*/
+
+            Console.WriteLine("곱셈 메서드 제거");
+            allCalc -= Multiple;
+            allCalc(10, 5);
+
+        }
+    }
+}
