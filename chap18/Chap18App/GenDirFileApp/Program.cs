52e70df37f87ec1b227c9322bc2fcfbba14b0e0a
JO HUI JI <whgmlwl222@naver.com>
2021-03-03 17:29:10 +0900

    18장 파일 읽기 쓰기

---
+++ chap18/Chap18App/GenDirFileApp/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+
+namespace GenDirFileApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string strDir = "D:\\Sources\\Gitrepository\\StudyCSharp21\\SampleDir"; // 폴더/파일 생성 테스트 폴더
+            string strTargetDir = "SubFolder"; // 생성할 폴더명
+            string strTargetFile = "readme.txt"; // 생성할 파일명
+
+            string targetPath = strDir + "\\" + strTargetDir; // "\\"이거 중간에 안쓰면 두 파일 경로가 합쳐져 버림
+            if (Directory.Exists(targetPath))
+            {
+                Console.WriteLine("이미 폴더가 존재합니다");
+            }
+            else
+            {
+                Directory.CreateDirectory(targetPath);
+                Console.WriteLine("폴더 생성 성공");
+            }
+
+
+            targetPath += $"\\{strTargetFile}";
+
+            if (File.Exists(targetPath))
+            {
+                Console.WriteLine("이미 파일이 존재합니다");
+            }
+            else
+            {
+                File.Create(targetPath).Close();
+                Console.WriteLine("파일 생성 성공");
+            }
+
+        }
+    }
+}
---
+++ chap18/Chap18App/TextFileApp/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextFileApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath = @"D:\Sources\Gitrepository\StudyCSharp21\SampleDir\SubFolder\a.dat"; // 텍스트파일 생성위치
+
+            StreamWriter sw = null;
+
+            try
+            {
+                sw = new StreamWriter(new FileStream(filePath, FileMode.OpenOrCreate, FileAccess.Write)); // 최초에는 create 나중에 추가는 Append
+                sw.WriteLine($"int.MaxValue = {int.MaxValue}");
+                sw.WriteLine("Hello, World!");
+                sw.WriteLine($"uint.MaxValue = {uint.MaxValue}");
+                sw.WriteLine("안녕하세요");
+                sw.WriteLine($"double.MaxValue = {double.MaxValue}");
+
+                Console.WriteLine("파일생성");
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine($"파일쓰기 예외발생 : {ex.Message}");
+            }
+            finally
+            {
+                sw.Close(); // 열었으면 꼭 닫기!!!
+            }
+
+            StreamReader sr = null;
+            sr = new StreamReader(new FileStream(filePath, FileMode.Open, FileAccess.Read));
+            Console.WriteLine($"File size : {sr.BaseStream.Length} bytes");
+
+            while (sr.EndOfStream == false)
+            {
+                Console.WriteLine(sr.ReadLine());
+            }
+            sr.Close();
+            
+        }
+    }
+}
