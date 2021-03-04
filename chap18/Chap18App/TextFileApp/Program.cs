66318fb824c2001937e6ba6beb397a75b40914b1
JO HUI JI <whgmlwl222@naver.com>
2021-03-04 09:21:52 +0900

    18장 파일처리 커밋

--- chap18/Chap18App/TextFileApp/Program.cs
+++ chap18/Chap18App/TextFileApp/Program.cs
@@ -32,20 +32,33 @@ namespace TextFileApp
                 Console.WriteLine($"파일쓰기 예외발생 : {ex.Message}");
             }
             finally
-            {
-                sw.Close(); // 열었으면 꼭 닫기!!!
+            {   
+                if(sw != null)
+                sw.Close(); // 열었으면 꼭 닫기!!! Close 안하면 에러남
             }
 
             StreamReader sr = null;
-            sr = new StreamReader(new FileStream(filePath, FileMode.Open, FileAccess.Read));
-            Console.WriteLine($"File size : {sr.BaseStream.Length} bytes");
-
-            while (sr.EndOfStream == false)
-            {
-                Console.WriteLine(sr.ReadLine());
-            }
-            sr.Close();
             
+            try
+            {
+                sr = new StreamReader(new FileStream(filePath, FileMode.Open, FileAccess.Read)); // Open과 같은 역할
+                Console.WriteLine($"File size : {sr.BaseStream.Length} bytes");
+
+                while (sr.EndOfStream == false)
+                {
+                    Console.WriteLine(sr.ReadLine());
+                }
+                sr.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"파일읽기 예외발생 : {ex.Message}");
+            }
+            finally
+            {
+                if (sw != null)
+                    sw.Close();
+            }
         }
     }
 }
